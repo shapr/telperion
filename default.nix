54c6615d09941df32fce3bd3b50688419e699aa0
Shae Erisson <shae@scannedinavian.com>
2022-09-01 10:59:08 -0400

    initial content

---
+++ default.nix
@@ -0,0 +1,43 @@
+{ compiler ? "ghc924" }:
+
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hself: hsuper: {
+      "takedouble" = hself.callCabal2nix "takedouble" (gitignore ./.) { };
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [ p."takedouble" ];
+    buildInputs = [
+      myHaskellPackages.haskell-language-server
+      pkgs.haskellPackages.cabal-fmt
+      pkgs.haskellPackages.cabal-install
+      pkgs.haskellPackages.ghcid
+      pkgs.haskellPackages.hasktags
+      pkgs.haskellPackages.hlint
+      pkgs.haskellPackages.ormolu
+      pkgs.niv
+      pkgs.nixpkgs-fmt
+    ];
+    withHoogle = true;
+  };
+
+  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."takedouble");
+
+  docker = pkgs.dockerTools.buildImage {
+    name = "takedouble";
+    config.Cmd = [ "${exe}/bin/takedouble" ];
+  };
+in {
+  inherit shell;
+  inherit exe;
+  inherit docker;
+  inherit myHaskellPackages;
+  "takedouble" = myHaskellPackages."takedouble";
+}
